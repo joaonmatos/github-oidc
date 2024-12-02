a3bbd17897120c682de8e8ee1f5ef412fafb11d6
João N. Matos <me@joaonmatos.com>
2024-12-02 08:24:20 +0100

    deregionalize app

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,9 @@
 import { App } from "aws-cdk-lib";
 import { GithubOidcStack } from "./github-oidc-stack";
 
-const account =
-  process.env.DEV_ACCOUNT_ID ??
-  process.env.AWS_ACCOUNT_ID ??
-  process.env.CDK_ACCOUNT_ID;
-const region =
-  process.env.DEV_REGION ?? process.env.AWS_REGION ?? process.env.CDK_REGION;
-
 const app = new App();
 new GithubOidcStack(app, "GithubOidcStack", {
   env: {
-    account,
-    region: region ?? "us-east-1",
+    region: "us-east-1",
   },
 });
